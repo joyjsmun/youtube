ba4cf0f5b70c7072beaf61b67dd0feaa2c166248
Joy <joy.js.mun@gmail.com>
2019-04-09 16:00:44 +0900

    ADD : install axios, connect with App.js, youtube api

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component{
+
+    searchTerm = (term) => {
+        console.log(term);
+    }
+
     render(){
         return(
             <div className="ui container" style={{marginTop:'10px'}}>
-                <SearchBar />
+                <SearchBar onSearch = {this.searchTerm}/>
             </div>
         )
     }
--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -7,10 +7,16 @@ class SearchBar extends React.Component{
         this.setState({term:e.target.value})
     }
 
+    submitHandler = (e) => {
+        e.preventDefault();
+
+        this.props.onSearch(this.state.term)
+    }
+
     render(){
         return(
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit = {this.submitHandler}>
                     <label>Search Video Clip here</label>
                     <input type="text" onChange={this.searchBarHandler} />
                 </form>
